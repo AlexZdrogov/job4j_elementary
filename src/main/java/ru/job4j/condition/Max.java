0b4c39e9d1dfbcfd95db932f6bc7ef6407ae69c1
Iskander <zdrogow@gmail.com>
2022-08-14 17:39:25 +0400

    3. Перегрузить метод max для трех чисел [#122644]

--- src/main/java/ru/job4j/condition/Max.java
+++ src/main/java/ru/job4j/condition/Max.java
@@ -4,4 +4,13 @@ public class Max {
     public static int summation(int first, int second) {
         return first > second ? first : second;
     }
+
+    public static int summation(int first, int second, int third) {
+        return summation(first, summation(second, third));
+    }
+
+    public static int summation(int first, int second, int third, int fourth) {
+        int temp = summation(second, third, fourth);
+        return summation(first, temp);
+    }
 }
--- src/test/java/ru/job4j/condition/MaxTest.java
+++ src/test/java/ru/job4j/condition/MaxTest.java
@@ -14,20 +14,23 @@ public class MaxTest {
     }
 
     @Test
-    public void whenMax2To1Then2() {
-        int left = 2;
-        int right = 1;
-        int result = Max.summation(left, right);
-        int expected = 2;
+    public void whenMax2To1To3Then3() {
+        int first = 2;
+        int second = 1;
+        int third = 3;
+        int result = Max.summation(first, second, third);
+        int expected = 3;
         Assert.assertEquals(result, expected);
     }
 
     @Test
-    public void whenMax2To2Then2() {
-        int left = 2;
-        int right = 2;
-        int result = Max.summation(left, right);
-        int expected = 2;
+    public void whenMax2To2To4To4Then4() {
+        int first = 2;
+        int second = 2;
+        int third = 4;
+        int fourth = 4;
+        int result = Max.summation(first, second, third, fourth);
+        int expected = 4;
         Assert.assertEquals(result, expected);
     }
 }
